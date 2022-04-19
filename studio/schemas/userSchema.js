50db43acc2c3770daae4b34ed071ddf81958b930
rashidwassaan <rashidwassaan@gmail.com>
2022-04-19 15:12:03 +0500

    sanity db setup

---
+++ studio/schemas/userSchema.js
@@ -0,0 +1,25 @@
+export const userSchema = {
+    name: 'users',
+    type: 'document',
+    title: 'users',
+    fields: [
+        {
+            name: 'name',
+            type: 'string',
+            title: 'Name',
+        },
+        {
+            name: 'walletAddress',
+            type: 'string',
+            title: 'Wallet Address',
+
+        }, 
+        {
+            name: 'profileImage',
+            type: 'string',
+            title: 'Profile Image',
+
+        }
+
+    ]
+} 
